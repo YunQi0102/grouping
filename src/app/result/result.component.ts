ae93151459b219829fd97159091cfecae6968fb6
2400616 <vickyzhang.systex.com>
2025-11-28 00:06:58 +0800

    1.新增顯示未抽/已抽號碼 2.防呆modal新增圖片

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -8,9 +8,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ResultComponent {
   @Input() drawNum!: number;
   @Input() remainingCount!: number;
+  @Input() drawnNumbers: number[] = [];
+  @Input() totalNumber!: number;
   @Output() nextDraw = new EventEmitter<void>();
 
   nextNumber() {
     this.nextDraw.emit();
+  }
+
+  isDrawn(num: number): boolean {
+    return this.drawnNumbers.includes(num);
+  }
+
+  getAllNumbers(): number[] {
+    return Array.from({ length: this.totalNumber }, (_, i) => i + 1);
   }
 }
